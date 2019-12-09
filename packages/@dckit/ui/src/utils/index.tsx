b0ec28289b362da36d7e482552ccac179990f3ac
Eugene Samonenko <ambroseus@gmail.com>
2019-12-09 15:59:26 +0200

    Update index.tsx

--- packages/@dckit/ui/src/utils/index.tsx
+++ packages/@dckit/ui/src/utils/index.tsx
@@ -10,11 +10,15 @@ export interface IMediaType {
   isMobile: boolean
 }
 
-const query = (max: number): boolean =>
-  useMediaQuery(`@media (max-width: ${max}px)`)
+const query = (max: number): string => `@media (max-width: ${max}px)`
 
-export const useMediaType = (): IMediaType => ({
-  isDesktop: query(922),
-  isTablet: query(768),
-  isMobile: query(576),
-})
+export const useMediaType = (): IMediaType => {
+  const isDesktop = useMediaQuery(query(922))
+  const isTablet = useMediaQuery(query(768))
+  const isMobile = useMediaQuery(query(576))
+  return {
+    isDesktop,
+    isTablet,
+    isMobile,
+  }
+}
