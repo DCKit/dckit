f770059db03f7a240b4b41fbd4723cd93009c8b7
Eugene Samonenko <esamonenko@agilevision.io>
2019-12-15 20:07:43 +0200

    added api-extractor for generation 'dist/typings.d.ts'

--- packages/@dckit/store/src/dck/selectors.ts
+++ packages/@dckit/store/src/dck/selectors.ts
@@ -1,5 +1,55 @@
-export * from '../items/selectors'
-export * from '../itemProps/selectors'
-export * from '../filters/selectors'
-export * from '../sorting/selectors'
-export * from '../processes/selectors'
+import {
+  getItems,
+  getItem,
+  getOptedItemId,
+  getOptedItem,
+  isItemSelected,
+  getSelectedItemIds,
+  getSelectedItems,
+} from '../items/selectors'
+
+import {
+  getItemProp,
+  getTotalItems,
+  getTotalPages,
+  getCurrentPage,
+  getPageSize,
+} from '../itemProps/selectors'
+
+import { getFilters, getFilter } from '../filters/selectors'
+
+import { getSortFields, getSortField } from '../sorting/selectors'
+
+import {
+  getProcess,
+  isProcessRunning,
+  isProcessFinished,
+  isProcessSucceed,
+  isProcessFailed,
+  getProcessResponse,
+} from '../processes/selectors'
+
+export const dckSelectors = {
+  getItems,
+  getItem,
+  getOptedItemId,
+  getOptedItem,
+  isItemSelected,
+  getSelectedItemIds,
+  getSelectedItems,
+  getItemProp,
+  getTotalItems,
+  getTotalPages,
+  getCurrentPage,
+  getPageSize,
+  getFilters,
+  getFilter,
+  getSortFields,
+  getSortField,
+  getProcess,
+  isProcessRunning,
+  isProcessFinished,
+  isProcessSucceed,
+  isProcessFailed,
+  getProcessResponse,
+}
