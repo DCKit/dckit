c2cb956cf7492b9f4297711d6bd9b6729d567a41
ambroseus <ambroseus@gmail.com>
2019-12-28 11:07:03 +0200

    added app test

--- packages/test-app/src/App.tsx
+++ packages/test-app/src/App.tsx
@@ -5,16 +5,13 @@ import { store } from './store'
 import { Items } from './components/Items'
 import { SideBar } from './components/SideBar'
 import { TestItem } from './items'
-import { AppLayout, AppBarHead, AppBarNav } from '@dckit/ui'
+import { AppLayout, AppBarHead } from '@dckit/ui'
 
 export const App: React.FC = () => (
   <Provider store={store}>
     <Router>
       <AppLayout>
         <AppBarHead.Provider>App</AppBarHead.Provider>
-        <AppBarNav.Provider>
-          <></>
-        </AppBarNav.Provider>
         <SideBar />
         <div style={{ padding: 16 }}>
           <Items itemType={TestItem} optedItemId={3} />
