351ce1a9ffe9c7c8e1e80d8f80afda7a4157524f
ambroseus <ambroseus@gmail.com>
2019-12-30 12:35:16 +0200

    added recursive renderRoutes

--- packages/test-app/src/components/Page1.tsx
+++ packages/test-app/src/components/Page1.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { AppBarNav, AppTabs } from '@dckit/ui'
 import { page1tabs } from '../pages'
 
-export const Page1: React.FC<{ props?: any }> = props => {
+export const Page1: React.FC<any> = props => {
   return (
     <>
       <AppBarNav.Provider>
-        <AppTabs tabs={page1tabs} path="/page1" />
+        <AppTabs tabs={page1tabs} path={props.match.url} />
       </AppBarNav.Provider>
       <h4>Page 1</h4>
       {props.children}
