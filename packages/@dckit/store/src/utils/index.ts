967f6ac35b5dbff6f22de2c85c3bdebf4fcfc090
Eugene Samonenko <ambroseus@gmail.com>
2020-01-23 16:34:34 +0200

    update deps

--- packages/@dckit/store/src/utils/index.ts
+++ packages/@dckit/store/src/utils/index.ts
@@ -1,12 +1,29 @@
-export const _get = (obj: any, path: string, defaultValue?: any): any => {
-  return String(path)
-    .split('.')
-    .reduce((acc, v) => {
-      try {
-        acc = acc[v]
-      } catch (e) {
-        return defaultValue
-      }
-      return acc
-    }, obj)
+interface AnyObject {
+  [key: string]: any
+}
+
+// lodash _.get replacement for objects only
+export const _get = (obj: AnyObject, path: string, defaultValue?: any): any => {
+  if (!obj || !path) return defaultValue
+
+  const parts = String(path).split('.')
+
+  if (parts.length === 1) {
+    let value
+    try {
+      value = obj[parts[0]]
+    } catch (e) {
+      return defaultValue
+    }
+    return value
+  }
+
+  return parts.reduce((acc, v) => {
+    try {
+      acc = acc[v]
+    } catch (e) {
+      return defaultValue
+    }
+    return acc
+  }, obj)
 }
