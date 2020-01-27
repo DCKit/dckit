8d654b6d6254fd2d1c87c6c10ca572a98028513c
ambroseus <ambroseus@gmail.com>
2020-01-27 11:54:40 +0200

    refactored render Form component

--- packages/@dckit/ui/src/forms/index.tsx
+++ packages/@dckit/ui/src/forms/index.tsx
@@ -27,82 +27,98 @@ export const Form = ({
   withDefaults = false,
   validationSchema,
 }: IFormProps) => {
-  const [useDefaults, setUseDefaults] = useState(
-    initialValues?.useDefaults || false
-  )
+  const [useDefaults, setUseDefaults] = useState(getUseDefaults())
   const form = useForm({ validationSchema, mode: 'onBlur' })
-  const { reset } = form
 
-  useEffect(() => {
+  useEffect(updateValues, [initialValues])
+
+  function getDefaultValues() {
+    return initialValues?.defaultValues || {}
+  }
+
+  function getUseDefaults() {
+    return initialValues?.useDefaults || false
+  }
+
+  function updateValues() {
+    const { reset } = form
     if (withDefaults) {
-      const values = initialValues?.defaultValues || {}
-      values.useDefaults = initialValues?.useDefaults || false
+      const values = getDefaultValues()
+      values.useDefaults = getUseDefaults()
       setUseDefaults(values.useDefaults)
       reset(values)
     } else {
       reset(initialValues)
     }
-  }, [reset, initialValues, withDefaults])
+  }
+
+  function resetDefaults(form: any, value: boolean) {
+    value && form.reset(getDefaultValues())
+    setUseDefaults(value)
+  }
+
+  function renderField(field: string) {
+    const config = fieldsConfig[field]
+    const { type = FormFieldType.text }: { type: FormFieldType } = config
+    const {
+      size = 12,
+      defaultValue = defaultByType[type],
+      controlProps,
+    } = config
+
+    const fieldProps = {
+      ...config,
+      form,
+      field,
+      useDefaults,
+    }
+
+    if (controlProps) form.watch(field, defaultValue)
+
+    return (
+      <Grid key={field} container item xs={12} sm={size}>
+        <Controller
+          as={<FormField {...fieldProps} />}
+          name={field}
+          control={form.control}
+          defaultValue={defaultValue}
+        />
+      </Grid>
+    )
+  }
+
+  function renderUseDefaults() {
+    return (
+      <Grid container>
+        <Grid item>
+          <Controller
+            as={
+              <FormField
+                form={form}
+                type={FormFieldType.switch}
+                field="useDefaults"
+                label="use defaults"
+                controlChange={resetDefaults}
+              />
+            }
+            name="useDefaults"
+            control={form.control}
+            defaultValue={false}
+          />
+        </Grid>
+        <Grid item container>
+          {renderActions(form)}
+        </Grid>
+      </Grid>
+    )
+  }
 
   return (
     <form>
       <Grid container spacing={4}>
-        {fields.map((field: string) => {
-          const config = fieldsConfig[field]
-          const { type = FormFieldType.text }: { type: FormFieldType } = config
-          const {
-            size = 12,
-            defaultValue = defaultByType[type],
-            controlProps,
-          } = config
-
-          if (controlProps) form.watch(field, defaultValue)
-
-          const fieldProps = {
-            ...config,
-            form,
-            field,
-            useDefaults,
-          }
-
-          return (
-            <Grid key={field} container item xs={12} sm={size}>
-              <Controller
-                as={<FormField {...fieldProps} />}
-                name={field}
-                control={form.control}
-                defaultValue={defaultValue}
-              />
-            </Grid>
-          )
-        })}
+        {fields.map(renderField)}
       </Grid>
-      {withDefaults && (
-        <Grid container>
-          <Grid item>
-            <Controller
-              as={
-                <FormField
-                  form={form}
-                  type={FormFieldType.switch}
-                  field="useDefaults"
-                  label="use defaults"
-                  controlChange={(form: any, value: boolean) => {
-                    value && form.reset(initialValues?.defaultValues)
-                    setUseDefaults(value)
-                  }}
-                />
-              }
-              name="useDefaults"
-              control={form.control}
-              defaultValue={false}
-            />
-          </Grid>
-          <Grid item container>
-            {renderActions(form)}
-          </Grid>
-        </Grid>
-      )}
+      {withDefaults && renderUseDefaults()}
       {!withDefaults && renderActions(form)}
     </form>
   )
