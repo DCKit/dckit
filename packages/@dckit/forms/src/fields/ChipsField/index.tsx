0c5917afe02d1733de13ff2851306bc46b155516
Eugene Samonenko <ambroseus@gmail.com>
2020-02-25 11:26:20 +0200

    added ChipToggle control

--- packages/@dckit/forms/src/fields/ChipsField/index.tsx
+++ packages/@dckit/forms/src/fields/ChipsField/index.tsx
@@ -1,65 +1,10 @@
 import React from 'react'
 import cn from 'clsx'
-import {
-  Grid,
-  FormControl,
-  FormLabel,
-  RadioGroup,
-  useFormControl,
-  Chip as MuiChip,
-} from '@material-ui/core'
-import { useField } from 'formik'
+import { Grid, FormControl, FormLabel, RadioGroup } from '@material-ui/core'
 import { MuiFieldProps } from '../../types'
+import { ChipToggle } from '../ChipToggle'
 import { HelperText } from '../HelperText'
 import { useStyles } from '../styles'
-
-const FocusDiv = React.forwardRef((props: any, ref: any) => {
-  const formControl = useFormControl()
-  return <div {...props} tabIndex={0} onBlur={formControl.onBlur} ref={ref} />
-})
-
-type RadioChipProps = {
-  name: string
-  label?: string
-  disabled?: boolean
-  small?: boolean
-  fullWidth?: boolean
-  value?: any
-}
-
-const RadioChip = React.memo((props: RadioChipProps) => {
-  const {
-    name,
-    label,
-    disabled,
-    small = false,
-    fullWidth = false,
-    value,
-  } = props
-  const classes = useStyles()
-  const { container, raised } = classes
-  const formControl = useFormControl()
-  const [field, , helpers] = useField(name)
-  const selected = field.value === value
-
-  const handleClick = () => {
-    formControl.onFocus()
-    !selected && helpers.setValue(value)
-  }
-
-  return (
-    <MuiChip
-      component={FocusDiv}
-      label={label}
-      color={selected ? 'primary' : 'default'}
-      size={small ? 'small' : 'medium'}
-      clickable={true}
-      disabled={disabled}
-      onClick={handleClick}
-      classes={{ root: cn(fullWidth && container, !selected && raised) }}
-    />
-  )
-})
 
 export const ChipsField = (props: MuiFieldProps) => {
   const classes = useStyles()
@@ -129,7 +74,7 @@ export const ChipsField = (props: MuiFieldProps) => {
               xs={size}
               className={chipPadding}
             >
-              <RadioChip {...chipProps} label={label} value={value} />
+              <ChipToggle radio {...chipProps} label={label} value={value} />
             </Grid>
           )
         })}
