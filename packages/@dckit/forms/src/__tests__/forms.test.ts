de2abb7d8507ee3581aabb134b3d964f70c6dfbb
ambroseus <ambroseus@gmail.com>
2020-03-08 08:34:00 +0200

    use screen in ui tests

--- packages/@dckit/forms/src/__tests__/forms.test.ts
+++ packages/@dckit/forms/src/__tests__/forms.test.ts
@@ -1,18 +1,18 @@
-import { cleanup, render, fireEvent } from '@testing-library/react'
+import { cleanup, screen, render, fireEvent } from '@testing-library/react'
 import { app } from './App'
 
 afterEach(cleanup)
 
 describe('form with defaults', () => {
   it('should render and use defaults', async () => {
-    const { getByText, getByLabelText, findAllByDisplayValue } = render(app)
+    render(app)
 
-    expect(getByText('submit')).toBeDefined()
-    const initial = await findAllByDisplayValue(/initial\s\S+/)
+    expect(screen.getByText('submit')).toBeDefined()
+    const initial = await screen.findAllByDisplayValue(/initial\s\S+/)
     expect(initial).toHaveLength(2)
 
-    fireEvent.click(getByLabelText('Use defaults'))
-    const defaults = await findAllByDisplayValue(/default\s\S+/)
+    fireEvent.click(screen.getByLabelText('Use defaults'))
+    const defaults = await screen.findAllByDisplayValue(/default\s\S+/)
     expect(defaults).toHaveLength(3)
   })
 })
