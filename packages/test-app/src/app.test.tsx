3f9c61172c692121ce86e8248e6991a5945ff02c
ambroseus <ambroseus@gmail.com>
2020-03-15 14:05:25 +0200

    fixed tests

--- packages/test-app/src/app.test.tsx
+++ packages/test-app/src/app.test.tsx
@@ -2,28 +2,31 @@ import React from 'react'
 import { screen, render, fireEvent } from '@testing-library/react'
 import { App } from './App'
 
+const { getByText, findByText, findAllByText } = screen
+
 describe('app', () => {
   it('should render and be clickable', async () => {
     render(<App />)
 
-    expect(screen.getByText('App')).toBeDefined()
+    expect(getByText('App')).toBeDefined()
 
-    fireEvent.click(screen.getByText('page1'))
-    expect(await screen.getByText('Page 1')).toBeDefined()
+    fireEvent.click(getByText('page1'))
+    expect(getByText('Page 1')).toBeDefined()
+    expect(await findAllByText(/Comp/)).toHaveLength(2)
 
-    fireEvent.click(screen.getByText('Tab 1'))
-    expect(await screen.getByText('Tab 1')).toBeDefined()
+    fireEvent.click(getByText('Tab 1'))
+    expect(getByText('Tab 1')).toBeDefined()
 
-    fireEvent.click(screen.getByText('Sub Tab 3'))
-    expect(await screen.getByText('Tab 1')).toBeDefined()
+    fireEvent.click(getByText('Sub Tab 3'))
+    expect(getByText('Tab 1')).toBeDefined()
 
-    fireEvent.click(screen.getByText('items'))
-    expect(await screen.findByText(/opted item/)).toBeDefined()
+    fireEvent.click(getByText('items'))
+    expect(await findByText(/opted item/)).toBeDefined()
 
-    fireEvent.click(screen.getByText('load items'))
-    expect(await screen.findAllByText(/item3/)).toBeDefined()
+    fireEvent.click(getByText('load items'))
+    expect(await findAllByText(/item3/)).toBeDefined()
 
-    fireEvent.click(screen.getByText('forms'))
-    expect(await screen.findByText(/Use defaults/)).toBeDefined()
+    fireEvent.click(getByText('forms'))
+    expect(await findByText(/Use defaults/)).toBeDefined()
   })
 })
