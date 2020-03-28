043c020875246923af43efdccec90955651841de
ambroseus <ambroseus@gmail.com>
2020-03-28 14:11:10 +0200

    added Flow.LoadAll

--- packages/@dckit/store/src/helpers/flows.ts
+++ packages/@dckit/store/src/helpers/flows.ts
@@ -1,14 +1,19 @@
 import { takeLatest } from 'redux-saga/effects'
 import { Acts, IAction } from '../types'
-import { Process } from './processes'
+import { Process, TProcess } from './processes'
 import { isAction } from './actions'
 
-function createFlow(act: Acts) {
-  function flow(itemType: string, saga: any, options?: any) {
+export const enum Flows {
+  LoadAll = 'LoadAll',
+}
+
+function createFlow(act: Acts, genericSaga?: any) {
+  function flow(itemType: string, flowSaga?: any, options?: any) {
     function* genericFlowSaga(action: IAction) {
-      const proc = Process.create(act)(action.meta.itemType, options)
+      const proc = Process.create(act)(itemType, options)
       yield proc.start()
       try {
+        const saga = genericSaga || flowSaga
         yield saga(proc, action)
         yield proc.stop()
       } catch (e) {
@@ -20,7 +25,13 @@ function createFlow(act: Acts) {
   return flow
 }
 
+function* loadAllSaga(proc: TProcess, action: IAction) {
+  yield proc.fetch(action)
+  yield proc.setItems(proc.data())
+}
+
 export const Flow = Object.freeze({
+  [Flows.LoadAll]: createFlow(Acts.Load, loadAllSaga),
   [Acts.Load]: createFlow(Acts.Load),
   [Acts.Add]: createFlow(Acts.Add),
   [Acts.Update]: createFlow(Acts.Update),
