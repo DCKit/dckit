fa6afd5f2fcf314d09a29fe7f7a03f1b2944c0fb
ambroseus <ambroseus@gmail.com>
2020-04-12 11:17:17 +0300

    fixed module declarations

--- packages/test-app/src/react-app-env.d.ts
+++ packages/test-app/src/react-app-env.d.ts
@@ -1,10 +1,3 @@
 /// <reference types="react-scripts" />
 
-declare module '@dckit/auth'
-declare module '@dckit/forms'
-declare module '@dckit/routes'
-declare module '@dckit/store'
-declare module '@dckit/tables'
-declare module '@dckit/ui'
-
 declare module 'comp'
