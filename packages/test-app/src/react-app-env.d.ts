df0e035fe72574554f212664a185531f125d860d
ambroseus <ambroseus@gmail.com>
2020-04-12 11:12:04 +0300

    use babelInclude & module-resolver for aliases

--- packages/test-app/src/react-app-env.d.ts
+++ packages/test-app/src/react-app-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="react-scripts" />
+
+declare module '@dckit/auth'
+declare module '@dckit/forms'
+declare module '@dckit/routes'
+declare module '@dckit/store'
+declare module '@dckit/tables'
+declare module '@dckit/ui'
+
+declare module 'comp'
